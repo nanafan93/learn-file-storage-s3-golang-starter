14cdfdfb66bf3644c48946fc93457331966bdcfc
nanafan93 <limayehnl2712@gmail.com>
2025-09-19 21:36:38 +1000

    start aws sdk

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	rand2 "crypto/rand"
+	"encoding/base64"
 	"fmt"
+	"io"
+	"mime"
 	"net/http"
+	"os"
+	"path/filepath"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
@@ -28,10 +34,61 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
-
-	// TODO: implement the upload here
-
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	file, header, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, 400, "too bad", err)
+		return
+	}
+	contentType := header.Header.Get("content-type")
+	mimeType, _, err := mime.ParseMediaType(contentType)
+	allowedTypes := map[string]bool{
+		"image/png":  true,
+		"image/jpeg": true,
+	}
+	if err != nil || !allowedTypes[mimeType] {
+		respondWithError(w, http.StatusBadRequest, "bad mime", err)
+		return
+	}
+	extensionForType, err := mime.ExtensionsByType(contentType)
+	if extensionForType == nil || err != nil {
+		respondWithError(w, http.StatusInternalServerError, "bad mime", err)
+		return
+	}
+	bytes := make([]byte, 32)
+	_, randErr := rand2.Read(bytes)
+	if randErr != nil {
+		respondWithError(w, http.StatusInternalServerError, "too bad", err)
+	}
+	randomFileName := base64.RawURLEncoding.EncodeToString(bytes)
+	filePath := filepath.Join(cfg.assetsRoot, fmt.Sprintf("%s%s", randomFileName, extensionForType[0]))
+	newFile, err := os.Create(filePath)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "too bad", err)
+		return
+	}
+	_, err = io.Copy(newFile, file)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "too bad", err)
+		return
+	}
+	thumbnailUrl := fmt.Sprintf("http://localhost:8091/assets/%s%s", randomFileName, extensionForType[0])
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "too bad", err)
+	}
+	video, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "cannot find video", err)
+		return
+	}
+	if userID != video.CreateVideoParams.UserID {
+		respondWithError(w, http.StatusUnauthorized, "not your video", err)
+		return
+	}
+	video.ThumbnailURL = &thumbnailUrl
+	if err := cfg.db.UpdateVideo(video); err != nil {
+		respondWithError(w, http.StatusInternalServerError, "sad times", err)
+		return
+	}
+	respondWithJSON(w, http.StatusOK, video)
 }
